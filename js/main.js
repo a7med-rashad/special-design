e6589ad241870cc8c28d077a82d1e71a769474af
a7med-rashad <a7medra4aad3@gmail.com>
2022-05-18 16:35:36 +0200

    continue changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+document.querySelector(".toggle-settings .fa-gear").onclick = function () {
+
+    this.classList.toggle("fa-spin");
+
+    document.querySelector(".settings-box").classList.toggle("open")
+};
+
+
+
+
 
 let imgs = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg", "06.jpg"];
 
